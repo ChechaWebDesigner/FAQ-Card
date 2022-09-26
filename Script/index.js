42b96a83df627588b3c3ead890ed0e73a57f8acd
César Joel González Oliva <113487509+ChechaWebDesigner@users.noreply.github.com>
2022-09-26 17:37:28 -0600

    Add files via upload

---
+++ Script/index.js
@@ -0,0 +1,101 @@
+let show = document.querySelectorAll(".show")
+let hidden = document.querySelectorAll(".hidden")
+let option = document.querySelectorAll(".options")
+let borderNone = document.querySelectorAll(".desactive-border")
+let img = document.querySelector(".img-box")
+
+let active = new Array(5).fill(false)
+
+const value = (node)=>{
+	for (no in node) {
+		if (isNaN(no) == false) {
+			if (node[no].classList.contains("active")) {
+				position = no;
+			return position;
+			}
+		}
+	}
+}
+
+
+show.forEach(button => {
+	button.addEventListener("click",()=>{
+		button.classList.add("active")
+		let validacion = button.classList.contains("active")
+		if (validacion) {
+			value(show)
+			active2 = active[position];
+			if (active2 == false) {
+				const intervalo = setInterval(()=>{
+					hidden[position].style.display = "block"
+					show[position].style.transform = "rotateX(180deg)"
+					option[position].setAttribute("style","color: #000; opacity: 1;")
+					borderNone[position].setAttribute("style","border: none; padding: none;")
+					active[position] = true
+				},150)
+				setTimeout(()=>{
+					clearInterval(intervalo)
+				},150)
+			} else {
+				hidden[position].style.display = "none"
+				show[position].style.transform = "rotateX(0deg)"
+				option[position].removeAttribute("style","color: #000; opacity: 1;")
+				borderNone[position].setAttribute("style","border-bottom: 1px solid #ccc;")
+				active[position] = false;
+			}
+		}	
+		button.classList.remove("active")
+	})
+})	
+
+option.forEach(element => {
+	element.addEventListener("mousemove",()=>{
+		img.style.animation = "move 2s linear forwards"	
+	})
+})
+
+option.forEach(element => {
+	element.addEventListener("mouseout",()=>{
+		img.style.animation = "reset 2s linear forwards"
+	})
+})
+
+
+
+
+
+// LA SOLUCION CON CICLO FOR
+
+//SIU
+
+/*
+
+for (let i=0; i < show.length; i++) {
+	show[i].addEventListener("click",()=>{
+		show[i].classList.add("active")
+		value(show)
+			active2 = active[position];
+			if (active2 == false) {
+				const intervalo = setInterval(()=>{
+					hidden[position].style.display = "block"
+					show[position].style.transform = "rotateX(180deg)"
+					option[position].setAttribute("style","color: #000; opacity: 1;")
+					borderNone[position].setAttribute("style","border: none; padding: none;")
+					active[position] = true
+				},150)
+				setTimeout(()=>{
+					clearInterval(intervalo)
+				},150)
+			} else {
+				hidden[position].style.display = "none"
+				show[position].style.transform = "rotateX(0deg)"
+				option[position].removeAttribute("style","color: #000; opacity: 1;")
+				borderNone[position].setAttribute("style","border-bottom: 1px solid #ccc;")
+				active[position] = false;
+			}
+		show[i].classList.remove("active")	
+	})
+}
+
+
+*/
